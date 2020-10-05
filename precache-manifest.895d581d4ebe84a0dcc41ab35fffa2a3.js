1768682e1ce164070c111fb8ef43ffed20a4f63f
himanshu20005 <himanshuparvanda@gmail.com>
2020-10-06 02:25:38 +0530

    Updates

---
+++ precache-manifest.895d581d4ebe84a0dcc41ab35fffa2a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3175a7c48b3c60e16d477c081ff9c3",
+    "url": "/IPL-data-visualization/index.html"
+  },
+  {
+    "revision": "cdd4cf0028c59d6513cd",
+    "url": "/IPL-data-visualization/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d4aa192674d1d4d41061",
+    "url": "/IPL-data-visualization/static/css/main.2215eae9.chunk.css"
+  },
+  {
+    "revision": "cdd4cf0028c59d6513cd",
+    "url": "/IPL-data-visualization/static/js/2.421bbe2a.chunk.js"
+  },
+  {
+    "revision": "dcd0ffc813fd4c2d67e951935a50067c",
+    "url": "/IPL-data-visualization/static/js/2.421bbe2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4aa192674d1d4d41061",
+    "url": "/IPL-data-visualization/static/js/main.cbd6d1c0.chunk.js"
+  },
+  {
+    "revision": "190ab172b3c05061816e",
+    "url": "/IPL-data-visualization/static/js/runtime-main.b4154f91.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/IPL-data-visualization/static/media/logo.ee7cd8ed.svg"
+  }
+]);
